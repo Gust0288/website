007760bc8a6488b9b54f3420f0861dfc94eb2ab9
Gustav <gubo0001@stud.kea.dk>
2023-06-06 17:06:31 +0200

    First commit

---
+++ js/modal.js
@@ -0,0 +1,72 @@
+
+var modal = document.getElementById("Modal_1");
+var modal2 = document.getElementById("Modal_2");
+var modal3 = document.getElementById("Modal_3");
+var modal4 = document.getElementById("Modal_4");
+var modal5 = document.getElementById("Modal_5");
+var modal6 = document.getElementById("Modal_6");
+
+
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+
+
+var span1 = document.querySelector(".close");
+var span2 = document.querySelector(".close2");
+var span3 = document.querySelector(".close3");
+var span4 = document.querySelector(".close4");
+var span5 = document.querySelector(".close5");
+var span6 = document.querySelector(".close6");
+
+
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+btn2.onclick = function() {
+  modal2.style.display = "flex";
+}
+btn3.onclick = function() {
+  modal3.style.display = "flex";
+}
+btn4.onclick = function() {
+  modal4.style.display = "flex";
+}
+btn5.onclick = function() {
+  modal5.style.display = "flex";
+}
+btn6.onclick = function() {
+  modal6.style.display = "flex";
+}
+
+
+span1.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+// -------
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     this.modal.style.display = "none";
+//   }
+// }
+
---
+++ js/nav.js
@@ -0,0 +1,36 @@
+const menu_btn = document.querySelector(".hamburger");
+
+const menu = document.querySelector(".nav-bar");
+
+
+function adActive(){
+    menu_btn.classList.toggle("is-active");
+}
+
+menu_btn.addEventListener("click", adActive);
+
+
+
+
+function toggleMenu(){
+    document.querySelector(".nav-bar").classList.toggle("shown");
+    document.querySelector("img").classList.toggle(".marg-top")
+    const menuShown = menu.classList.contains("shown");
+
+}
+
+
+
+function shake(){
+    document.querySelector(".hamburger").classList.add("shake");
+}
+function shakeNot(){
+    document.querySelector(".hamburger").classList.remove("shake");
+}
+
+
+
+menu_btn.addEventListener("mouseover", shake);
+menu_btn.addEventListener("mouseout", shakeNot);
+
+menu_btn.addEventListener("click", toggleMenu);
